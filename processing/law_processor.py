447d6526f6f73908d49cc13ca9e03ad3939d1299
goodluckjw <chetera@naver.com>
2025-04-25 00:52:03 +0900

    Update law_processor.py

--- processing/law_processor.py
+++ processing/law_processor.py
@@ -150,7 +150,8 @@ def run_amendment_logic(find_word, replace_word):
         loc_str = format_location_groups(raw_locations)
         각각 = "각각 " if len(raw_locations) > 1 else ""
         sentence = (
-            f"{unicircle(idx+1)} {law_name} 일부를 다음과 같이 개정한다.<br>&nbsp;&nbsp;"
+            f"{unicircle(idx+1)} {law_name} 일부를 다음과 같이 개정한다.<br>"
+             #  f"{unicircle(idx+1)} {law_name} 일부를 다음과 같이 개정한다.<br>&nbsp;&nbsp;" 로 하면 줄바꾸고 두칸 띄움
             f"{loc_str} 중 “{find_word}”{조사} {각각}“{replace_word}”로 한다."
         )
         amendment_results.append(sentence)
